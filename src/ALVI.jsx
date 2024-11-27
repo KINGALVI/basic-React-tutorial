65fe1a334afe9df7c0297cebabadace3fc699f4c
KINGALVI <alvialmahamud@gmail.com>
2024-11-28 00:29:20 +0200

    edit some code

--- src/ALVI.jsx
+++ src/ALVI.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import { useEffect, useState } from "react"
+import './ALVI.css'
 
 export default function ALVI() {
     return (
@@ -27,11 +28,20 @@ function TotalCountryAPI() {
             })
     }, [])
 
+    // const [visitedCountry , setVisitedCountry] = useState([]);
+
+    const handelVisitedCountrys = (countryList) => {
+        alert('alvi', countryList)
+    }
+
     return (
         <section>
+            <h1>Countrys That I Visited !!</h1>
+            <ul>
 
+            </ul>
             {
-                country.length > 0 ? country.map(country => <CountryAPIDetail key={country.cca2} allCountry={country}></CountryAPIDetail>) : <h1> LODING PAGE....</h1>
+                country.length > 0 ? country.map(country => <CountryAPIDetail key={country.cca2} handelVisitedCountrys={handelVisitedCountrys} allCountry={country}></CountryAPIDetail>) : <h1> LODING PAGE....</h1>
             }
 
         </section>
@@ -39,20 +49,37 @@ function TotalCountryAPI() {
 }
 
 //Destructuring api
-function CountryAPIDetail({ allCountry }) {
+function CountryAPIDetail({ allCountry, handelVisitedCountrys }) {
 
     const { name, region, subregion, capital, flags, population } = allCountry;
 
+    const [visited, setVisited] = useState(false);
+
+    const handelVisited = () => {
+        setVisited(!visited);
+    }
+
     return (
-        <div>
+        // example of using conditional class
+        <div className={visited ? 'visited-country-containar' : 'visit-country-containar'}>
             <hr />
+
             <img src={flags.png} alt={flags.alt} />
-            <h2>{name.common}</h2>
+            <h2 style={{ backgroundColor: visited ? 'purple' : 'black' }}>{name.common}</h2>
             <h3>Official Name : {name.official}</h3>
             <h3>Capital : {capital[0]}</h3>
             <h3>Region : {region}</h3>
             <h3>Region : {subregion}</h3>
             <h3>Population : {population}</h3>
+
+            <button onClick={handelVisitedCountrys}>Mark As Visited</button>
+
+            <br /> <br />
+            {/* example of using conditional button */}
+            <button onClick={handelVisited}>{visited ? 'visit Done' : 'Visit'}</button>
+            <br /> <br />
+            {visited ? "You have visited this country !!" : "You haven't visited this country"}
+
             <hr />
         </div>
     )
